cf5e09a2c4037d264204404a63df918eb43a795b
yyyaa1569000 <yyaa1569@gmail.com>
2026-09-07 23:51:24 +0800

    fix: 更新網頁內容(測試2)

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('team-log');
+  protected readonly title = signal('奈奈');
 }
