f23ebfcd70305b01004046afbb6fc28b8b8c3cef
yyyaa1569000 <yyaa1569@gmail.com>
2026-09-07 23:44:25 +0800

    fix: 更新網頁內容(測試)

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, team-log');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 奈奈');
   });
 });
